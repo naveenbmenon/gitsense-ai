5042fea137f293390856bffe5040581df586093e
naveenbmenon <naveenbijulalmenon@gmail.com>
2026-02-10 23:47:38 +0530

    Fix CORS issue to allow deployed frontend

--- app/main.py
+++ app/main.py
@@ -12,7 +12,8 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "http://127.0.0.1:5173"
+        "http://127.0.0.1:5173",
+        "https://gitsense-ai-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
