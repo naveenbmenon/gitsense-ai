7e66a9b8381b5dfe81d67e67e44759f122b5a06b
naveenbmenon <naveenbijulalmenon@gmail.com>
2026-02-11 18:35:57 +0530

    Changed CORS URL for frontend after new deployment

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "http://127.0.0.1:5173",
-        "https://gitsense-ai.vercel.app"  # 👈 FRONTEND URL
+        "https://gitsense-ai-dashboard.vercel.app/"  # 👈 FRONTEND URL
     ],
     allow_credentials=True,
     allow_methods=["*"],
